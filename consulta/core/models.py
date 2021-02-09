81659eeafdb388237d989d5709c6cb67d52c775e
Marcelo Schneider <schneider.fei@gmail.com>
2021-02-09 18:20:30 -0300

    Adiciona ParticipantesModel

--- consulta/core/models.py
+++ consulta/core/models.py
@@ -5,3 +5,10 @@ from django.utils.translation import gettext as _
 class CursoModel(models.Model):
     nome = models.CharField(_("nome"), max_length=254)
     num_vagas = models.IntegerField(_("Número de vagas"))
+
+
+class ParticipantesModel(models.Model):
+    nome = models.CharField(_("Nome"), max_length=128)
+    posicao = models.IntegerField(_("Posicao"))
+    status = models.CharField(_("Status"), max_length=30, null=True)
+    curso = models.ForeignKey('CursoModel', on_delete=models.CASCADE)
--- consulta/core/tests/test_core_models.py
+++ consulta/core/tests/test_core_models.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.db import models
 
 from ..models import CursoModel
+from ..models import ParticipantesModel
 
 
 class CursoModelTest(TestCase):
@@ -21,3 +22,25 @@ class CursoModelTest(TestCase):
         data = dict(nome='Princípios meteorologia', num_vagas=50)
         CursoModel.objects.create(**data)
         self.assertTrue(CursoModel.objects.exists())
+
+
+class ParticipantesModelTest(TestCase):
+    def setUp(self):
+        self.obj = ParticipantesModel()
+
+    def test_is_instance_of_models(self):
+        self.assertIsInstance(self.obj, ParticipantesModel)
+
+    def test_has_attributes(self):
+        attributes = ('posicao', 'nome', 'status')
+        for attr in attributes:
+            with self.subTest():
+                self.assertTrue(hasattr(self.obj, attr))
+
+    def test_data_exists(self):
+        curso_data = dict(nome='Princípios da Meteorologia', num_vagas=50)
+        curso = CursoModel.objects.create(**curso_data)
+        participante_data = dict(
+            nome='Abreu', posicao=2, status='Aceito', curso=curso)
+        ParticipantesModel.objects.create(**participante_data)
+        self.assertTrue(ParticipantesModel.objects.exists())
