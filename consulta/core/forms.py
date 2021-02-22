18480b88cef9645ec8addfbb47981d74ff9e0daa
Marcelo Schneider <schneider.fei@gmail.com>
2021-02-21 22:46:58 -0300

    Adiciona atulização de participante #2
    Permite que dados de participantes sejam atualizados

--- consulta/core/forms.py
+++ consulta/core/forms.py
@@ -6,3 +6,9 @@ class CursoForm(forms.Form):
                            max_length=128, required=True)
     num_vagas = forms.IntegerField(label='Número de vagas', required=True)
     file = forms.FileField(label='Arquivo Csv', required=True)
+
+
+class ParticipanteForm(forms.Form):
+    nome = forms.CharField(label='Nome', max_length=128, required=True)
+    posicao = forms.IntegerField(label='Posicao', required=True)
+    status = forms.CharField(label='Status', max_length=30, required=True)
